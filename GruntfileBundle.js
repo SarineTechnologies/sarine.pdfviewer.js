b4b80e38c0e9b19b14ee6a0e6d70e384e989cb0e
Unknown <rivkas@3base.co.il>
2018-01-24 15:38:53 +0200

    fix grunt file

--- GruntfileBundle.js
+++ GruntfileBundle.js
@@ -45,7 +45,7 @@ module.exports = function(grunt) {
         if(process.env.buildFor == 'deploy')
         {
             return {
-                build: 'dist/build'
+                build: 'dist'
              }
         }
         else
